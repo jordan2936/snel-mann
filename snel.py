5ceea6f15c168899c4036d61534df11192617f5f
Faith Talley <talleyfaith@gmail.com>
2018-02-09 13:54:19 -0500

    Completed over half of assignmnet

--- snel.py
+++ snel.py
@@ -1,6 +1,6 @@
 import random
 
-def generate_random_text(num_chars):
+def generate_random_text(num_chars): # returns a variable with random txt of length num_chars
     text = ""
 
     for i in range(num_chars):
@@ -10,36 +10,54 @@ def generate_random_text(num_chars):
     return text
 
 
-def mean(counts):
-    pass
+def mean(counts): # gets the mean of a list that has to do with unicode stuff
+    total = 0
+    for c in counts:
+        character = chr(c)
+        total += ord(character)
 
-    return 0
+    avg = total / len(counts)        
+        
+    return avg
 
-def chi_square(counts):
+def chi_square(counts): # calculates chi square with unicode stuff
     expected = mean(counts)
     
     X = 0
 
     for n in counts:
-        pass
+        character = chr(n)
+        observed = ord(character)
+        top = observed - expected
+        X += (top)**2
 
-    return X
+    return X/expected
 
+def count_characters(text, counts): # counts the characters
+    for c in text:
+        n = ord(c)
+        counts[n] += 1
 
-
-bunch_of_text = "asdfhjkjaewhfkljsadhflkjhasedfi9835u2xifu-q3ymtocfjd;sk^%$#^YJH&I*^%$*%TNIOUYTYU^RYI^RGLKHNJJKYTB"
-
-counts = [0] * 127
-print(counts)
-
-
-for c in bunch_of_text:
-    n = ord(c)
-    counts[n] += 1
-
-print(counts)
-
-
-content = generate_random_text(100)
+content = generate_random_text(15000)
 
 # code here to write content to a file
+
+with open("random_text.txt", "w+") as f:
+    f.write(content)
+
+counts = [0] * 127
+count_characters(content, counts)
+print(counts)
+print(chi_square(counts))
+
+content = generate_random_text(5000)
+content += 
+# code here to write content to a file
+
+with open("random_text.txt", "w+") as f:
+    f.write(content)
+
+counts = [0] * 127
+count_characters(content, counts)
+print(counts)
+print(chi_square(counts))
